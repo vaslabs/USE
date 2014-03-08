9e920897b759e10a06944ef744f3e18d4928b50f
Angelos Georgiadis <georgim0@localhost.localdomain>
2014-03-08 20:45:35 +0000

    python library. python setup.py install in sdk

--- facebookSrc/FacebookConfig.py
+++ facebookSrc/FacebookConfig.py
@@ -3,9 +3,9 @@
 from facepy.utils import get_extended_access_token
 import facebook
 
-app_id = 294242907396048
-app_secret = 'b123ce2b1161c2f6747e5a401adefc49'
-short_lived_access_token = "CAAELnMP5F9ABACmjpyKvIk66KtYAZC3TvHzZBbyO39AkH0MIqdVQiKPnGzS7YeAT0D44GyZCPUpH0cZCjLN2cf8hj0CPWRF04ZCssBLZAAq6KZBo3aBHseYZBPYQSX0KdsX2JkYqB35VEkvSgzyQFxC6zGvtkStSEj8Wif40oHhbKY6T2Agb0jju0XeMNZBv21xUZD"
+app_id = XXXXXXX
+app_secret = 'XXXXXX'
+short_lived_access_token = "XXXXXX" #get this from https://developers.facebook.com/tools/explorer/ by selecting the application and pasting the long token
 try :
     long_lived_access_token, expires_at = get_extended_access_token(
             short_lived_access_token,
